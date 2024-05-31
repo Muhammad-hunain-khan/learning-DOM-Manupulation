085b01fbbfd970e40d15eec56934691be31a866e
hunii ali <huniiali1122@gmail.com>
2024-05-31 16:59:37 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,75 @@
+
+
+
+// var hunii = document.querySelector("h1")
+//     hunii = console.log("hello jan kese ho");
+
+// var h1 = document.querySelector("h1")
+// h1.innerHTML = "bado badi";
+
+// var p = document.querySelector("p")
+// p.innerHTML = " how are you";
+
+// h1.style.textAlign = "center"
+// h1.style.fontSize = "60px"
+// p.style.textAlign = "center"
+// p.style.fontSize = "60px"
+// p.style.backgroundColor = "black"
+// p.style.color = "white"
+// h1.style.backgroundColor = "black"
+// h1.style.color = "white"
+
+// h1.addEventListener("click",function(){
+//    h1.innerHTML = "ja na bc",
+//    h1.style.backgroundColor = "grey",
+//    h1.style.color = "white"
+   
+
+// })
+
+// p.addEventListener("click",function(){
+//     p.innerHTML = "abhi tk nh gaya",
+//     p.style.backgroundColor = "grey",
+//     p.style.color = "white"
+// })
+
+
+
+
+// ------this is an little light roundcircle just click then color changed--------
+
+// var circle = document.querySelector("#circle");
+// var button = document.querySelector("button");
+
+// var hunain = 0; 
+
+// button.addEventListener("click",function(){
+//     if(hunain == 0){
+//         circle.style.backgroundColor = "red"
+//         button.innerHTML = "OFF"
+//         console.log("on hogya")
+//         hunain = 1
+        
+
+//     }else{
+//         circle.style.backgroundColor = "white"
+//         button.innerHTML = "on"
+//         hunain = 0
+//         console.log("off hogya")
+
+//     }
+
+// })
+
+
+//Certainly! Let's clarify the difference between innerHTML and textContent in JavaScript and provide some examples for using each.
+
+
+
+
+// -----------select all h1 ----------------
+
+var h1 = document.querySelectorAll("h1");
+ h1 = console.log(h1)
+
+//  then run this program result in node list what is modelist: nodelist is similar to array
